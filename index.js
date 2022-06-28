eba2a900833d65033f46e4fa85621eaf6f45dd2f
Agnes <agsawicka@gmail.com>
2022-06-28 09:29:11 -0400

    commit tree 463631e0e62b57776cc9283c804a0818bf3fadb8

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+let currentUser = 'Grace Hopper';
+let welcomeMessage = "Welcome to Flatbook, " + currentUser+ "!";
+let shortGreeting = "Welcome, " + currentUser[0]+"!";
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
